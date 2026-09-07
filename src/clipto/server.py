7a2af384ca159b734bdba438107be8b4881135d7
Matt Hills <mattintech@gmail.com>
2026-09-07 12:50:58 -0400

    Remove Access-Control-Allow-Origin wildcard header

--- src/clipto/server.py
+++ src/clipto/server.py
@@ -310,7 +310,6 @@ class CliptoRequestHandler(BaseHTTPRequestHandler):
         self.send_response(status)
         self.send_header("Content-Type", "application/json; charset=utf-8")
         self.send_header("Content-Length", str(len(body)))
-        self.send_header("Access-Control-Allow-Origin", "*")
         if cookie:
             self.send_header("Set-Cookie", cookie)
         self.end_headers()
